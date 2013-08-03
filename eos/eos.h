7fa9c7cb298a64934dbae7b1ba7432c35110830d
Fabio Cesar Canesin <fabio.canesin@gmail.com>
2013-08-03 15:49:46 -0300

    Replace std::function with function pointers, add openprop namespace, add backward diff, second diff and fix some typos.

---
+++ eos/eos.h
@@ -0,0 +1,16 @@
+//  OpenPROP - Open source properties library
+//	Fabio Cesar Canesin <fabio.canesin@gmail.com>
+//	MIT 2013 license
+//	eos.h - Equations of state for OpenPROP
+
+#ifndef EOS_H
+#define EOS_H
+
+namespace openprop
+{
+	
+
+
+} /* openprop */
+
+#endif /* end of include guard: EOS_H */
--- utils/utils.h
+++ utils/utils.h
@@ -1,12 +1,15 @@
-#ifndef UTILS_H
-#define UTILS_H
-
 //  OpenPROP - Open source properties library
 //	Fabio Cesar Canesin <fabio.canesin@gmail.com>
 //	MIT 2013 license
 //	utils.h - Utitilities for OpenPROP
-#include <functional>
+
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <limits>
+
+namespace openprop
+{
 
 //REAL is the definition for the precision in the algorithms of the library
 //this forces the REAL, utils should prevail over local definitions
@@ -26,33 +29,47 @@
 #endif
 
 
-//Central difference function, makes two function calls, O(h^2)
-REAL diff_central(const REAL& h, const REAL& x, const std::function<REAL (const REAL&)>& func)
+//Central diff function, makes two function calls, O(h^2)
+REAL diff(const REAL& h, const REAL& x, const REAL (*func)(const REAL&))
 {
 	// diff = f(x + h) - f(x -h)/2h + O(h^2)
-	return (func(x + h) - func(x - h))/(2.0*h + std::numeric_limits<REAL>::min());
+	return ((*func)(x + h) - (*func)(x - h))/(2.0*h + std::numeric_limits<REAL>::min());
 }
 
-//Foward difference function, receive funcx making only one function call, O(h)
-REAL diff_foward(const REAL& h, const REAL& x, const REAL& funcx, const std::function<REAL (const REAL&)>& func)
+//Forward diff function, receive funcx making only one function call, O(h)
+REAL diff_forward(const REAL& h, const REAL& x, const REAL& funcx, const REAL (*func)(const REAL&))
 {
 	// diff = f(x + h) - f(x)/h + O(h)
-	return (func(x + h) - funcx)/(h + std::numeric_limits<REAL>::min());
+	return ((*func)(x + h) - funcx)/(h + std::numeric_limits<REAL>::min());
+}
+
+//Backward diff function, receive funcx making only one function call, O(h)
+REAL diff_backward(const REAL& h, const REAL& x, const REAL& funcx, const REAL (*func)(const REAL&))
+{
+	// diff = f(x) - f(x - h)/h + O(h)
+	return (funcx - (*func)(x - h))/(h + std::numeric_limits<REAL>::min());
+}
+
+//Central second diff function, receive funcx making only two function calls, O(h**2)
+REAL diff2(const REAL& h, const REAL& x, const REAL& funcx, const REAL (*func)(const REAL&))
+{
+	// diff2 = f(x +h) +2*f(x) - f(x - h)/h**2 + O(h**2)
+	return ((*func)(x + h) + 2*funcx - (*func)(x - h))/(h + std::numeric_limits<REAL>::min());
 }
 
 //	Possitive unidimensional Newton-Raphson-Bissection algorithm
-//	Use numerical foward differencing for speed (reduces number of function calls)
+//	Use numerical forward differencing for speed (reduces number of function calls)
 //	Use a initial guess when calling PNRB, like ideal gas equation solution for variable
-REAL PNRB(REAL guess, const std::function<REAL (const REAL&)>& func)
+REAL PNRB(REAL guess, const REAL (*func)(const REAL&))
 {
-	REAL fx = func(guess);
+	REAL fx = (*func)(guess);
 	//Early-abort, check if guess is already root for equation
 	if (fx < 1E-12 && guess > 0.0) return guess;
 	//Lower limit for real space. Searching for a non-negative root
 	REAL a = std::numeric_limits<REAL>::min();
 	//Upper limit for real space in REAL type
 	REAL b = std::numeric_limits<REAL>::max();
-	REAL fa = func(a);
+	REAL fa = (*func)(a);
     int count = 0;
 	do {
 		//If root is in [a, guess] reduces search space around it.
@@ -64,11 +81,11 @@ REAL PNRB(REAL guess, const std::function<REAL (const REAL&)>& func)
 			a = guess;
 		}
 		//Choose new guess using Newton approximation
-		guess = guess - fx/diff_foward(1.0E-5, guess, fx, func);
+		guess = guess - fx/diff_forward(1.0E-5, guess, fx, func);
 		//If the result is outside braket use bissection step (for stability)
 		if ((b-guess)*(guess-a) < 0.0) guess = guess + 0.5*(b-a);
 		//Compute new value for function
-		fx = func(guess);
+		fx = (*func)(guess);
 		//Check if guess is root for equation
 		if (fx < 1E-12 && guess > 0.0) return guess;
 		//Increase interation count
@@ -77,5 +94,7 @@ REAL PNRB(REAL guess, const std::function<REAL (const REAL&)>& func)
 	} while(count < 101);
 	return guess;
 }
+
+} /* openprop */
 
 #endif /* end of include guard: UTILS_H */
